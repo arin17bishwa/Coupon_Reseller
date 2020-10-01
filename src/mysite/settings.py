64368137cd16721649c559baf93540713dcbd288
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-01 23:22:30 +0530

    Update settings.py
    removed useless secret key

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get("DJ_SECRET_KEY_COUPON_RESELLER") #'t&m(g+s#haha)h@0u8-giz_3mixdht55wr4)uz6hp^xxrt@(^='
+SECRET_KEY =os.environ.get("DJ_SECRET_KEY_COUPON_RESELLER") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
